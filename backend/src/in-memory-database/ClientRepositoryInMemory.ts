ce5634dd983550cd5ad4601fcf4b3ef28f988b68
Jean F. dos Santos <jeanddg@hotmail.com>
2023-01-01 17:51:10 -0300

    feat: Implement method findAll

--- backend/src/in-memory-database/ClientRepositoryInMemory.ts
+++ backend/src/in-memory-database/ClientRepositoryInMemory.ts
@@ -1,9 +1,21 @@
 import { randomUUID } from 'node:crypto';
 import { Client } from '../entities/Client/Client';
-import { IClientRepository } from '../interfaces/IClientRepository';
+import {
+  findAllProps,
+  FindAllReturn,
+  IClientRepository,
+} from '../interfaces/IClientRepository';
 
 export class ClientRepositoryInMemory implements IClientRepository {
   items: Client[] = [];
+
+  async findAll(props: findAllProps): Promise<FindAllReturn> {
+    return {
+      count: this.items.length,
+      limit: 4,
+      clients: this.items,
+    };
+  }
 
   async findByWhatsapp(whatsapp: string): Promise<Client | boolean> {
     const client = this.items.find((c) => c.whatsapp === whatsapp);
--- backend/src/interfaces/IClientRepository.ts
+++ backend/src/interfaces/IClientRepository.ts
@@ -1,6 +1,17 @@
 import { Client } from '../entities/Client/Client';
 
+export interface FindAllReturn {
+  clients: Client[];
+  count: number;
+  limit: number;
+}
+
+export interface findAllProps {
+  page: number;
+}
+
 export interface IClientRepository {
   create(client: Client): Promise<Client>;
   findByWhatsapp(whatsapp: string): Promise<Client | boolean>;
+  findAll(props: findAllProps): Promise<FindAllReturn>;
 }
