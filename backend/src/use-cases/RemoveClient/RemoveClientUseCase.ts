ae3fff658a1f9df98e0bebf70c3e24b65366774f
Jean F. dos Santos <jeanddg@hotmail.com>
2023-01-01 18:30:14 -0300

    feat: add class RemoveClientUseCase

---
+++ backend/src/use-cases/RemoveClient/RemoveClientUseCase.ts
@@ -0,0 +1,18 @@
+import { CustomException } from '../../exceptions/CustomException';
+import { IClientRepository } from './../../interfaces/IClientRepository';
+
+interface FindAllClientsUseCaseProps {
+  clientId: string;
+}
+
+export class RemoveClientUseCase {
+  constructor(private clientRepository: IClientRepository) {}
+
+  async execute({ clientId }: FindAllClientsUseCaseProps) {
+    const client = await this.clientRepository.findById(clientId);
+
+    if (!client) throw new CustomException('Client not exists!');
+
+    await this.clientRepository.remove(clientId);
+  }
+}
