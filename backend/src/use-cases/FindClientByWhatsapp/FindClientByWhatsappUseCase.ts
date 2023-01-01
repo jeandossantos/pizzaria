245878b7ff5d33bb9a434e408063a2f244acd4ed
Jean F. dos Santos <jeanddg@hotmail.com>
2023-01-01 16:45:07 -0300

    feat: add class FindClientByWhatsappUseCase

---
+++ backend/src/use-cases/FindClientByWhatsapp/FindClientByWhatsappUseCase.ts
@@ -0,0 +1,22 @@
+import { CustomException } from '../../exceptions/CustomException';
+import { IClientRepository } from '../../interfaces/IClientRepository';
+
+interface FindClientByWhatsappUseCaseProps {
+  whatsapp: string;
+}
+
+export class FindClientByWhatsappUseCase {
+  constructor(private clientRepository: IClientRepository) {}
+
+  async execute({ whatsapp }: FindClientByWhatsappUseCaseProps) {
+    const client = await this.clientRepository.findByWhatsapp(whatsapp);
+
+    if (!client) {
+      throw new CustomException('User not found!');
+    }
+
+    return {
+      client,
+    };
+  }
+}
