0348abf999832dcbadec72d45ccb726dee8c70bc
Jean F. dos Santos <jeanddg@hotmail.com>
2023-01-01 17:51:59 -0300

    test: add test find all clients

---
+++ backend/src/use-cases/FindAllClients/FindAllClientsUseCase.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from '@jest/globals';
+
+import { ClientRepositoryInMemory } from '../../in-memory-database/ClientRepositoryInMemory';
+
+import { FindAllClientsUseCase } from './FindAllClientsUseCase';
+
+const clientRepository = new ClientRepositoryInMemory();
+
+describe('Find All Clients', () => {
+  it('should find all clients', async () => {
+    const findAllClientsUseCase = new FindAllClientsUseCase(clientRepository);
+
+    const result = await findAllClientsUseCase.execute({
+      page: 1,
+    });
+
+    expect(result.clients).toBeInstanceOf(Array);
+    expect(result).toHaveProperty('count');
+    expect(result).toHaveProperty('limit');
+  });
+});
