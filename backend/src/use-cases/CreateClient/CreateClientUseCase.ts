ae8890889509740e270e85928099315d9854d930
Jean F. dos Santos <jeanddg@hotmail.com>
2023-01-02 17:09:00 -0300

    feat: validate Client before create

--- backend/src/use-cases/CreateClient/CreateClientUseCase.ts
+++ backend/src/use-cases/CreateClient/CreateClientUseCase.ts
@@ -1,3 +1,5 @@
+import { ClassValidadorError } from './../../exceptions/ClassValidadorError';
+import { validate } from 'class-validator';
 import { Address } from '../../entities/Address/Address';
 import { Client } from '../../entities/Client/Client';
 import { IClientRepository } from '../../interfaces/IClientRepository';
@@ -15,6 +17,12 @@ export class CreateClientUseCase {
         ...address,
       }),
     });
+
+    const errors = await validate(client);
+
+    if (errors.length > 0) {
+      throw new ClassValidadorError(errors);
+    }
 
     const newClient = await this.clientRepository.create(client);
 
